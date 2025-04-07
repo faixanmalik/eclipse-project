52c003efa5b83913d96a95cb7452c2897ab7529d
Faizan Malik <faixan.malik554@gmail.com>
2025-04-07 14:37:49 +0500

    gallery update

--- app/website-design.tsx
+++ app/website-design.tsx
@@ -15,93 +15,46 @@ const WebsiteDesign = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-10">
+
+          {/* sample 1-3 */}
           <div className="grid gap-4">
               <div>
-              <video
-                width="500"
-                height="500"
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="h-auto max-w-full rounded-lg"
-              >
-                <source src="/assets/video1.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              </div>
-              <div>
-                  <Image
+                <Image
                   width={500}
                   height={500}
                   priority
-                  src="/assets/sample1.jpg" 
-                  alt=""
-                  className="h-auto max-w-full rounded-lg" />
-              </div>
-              <div>
-                  <Image
-                  width={500}
-                  height={500}
-                  priority
-                  className="h-auto max-w-full rounded-lg"
                   src="/assets/sample3.jpg" 
-                  alt=""/>
+                  alt=""
+                  className="h-72 object-cover object-top max-w-full rounded-lg" />
+              </div>
+              <div>
+                <Image
+                  width={500}
+                  height={500}
+                  priority
+                  src="/assets/sample4.jpg" 
+                  alt=""
+                  className="h-72 object-cover object-top max-w-full rounded-lg" />
+              </div>
+              <div>
+                <Image
+                  width={500}
+                  height={500}
+                  priority
+                  src="/assets/sample5.jpg" 
+                  alt=""
+                  className="h-72 object-cover object-top max-w-full rounded-lg" />
               </div>
           </div>
+
+           {/* sample 4-2 */}
           <div className="grid gap-4">
               <div>
-                  <Image
+                <Image
                   width={500}
                   height={500}
                   priority
-                  className="h-auto max-w-full rounded-lg" 
-                  src="/assets/sample5.jpg" alt=""/>
-              </div>
-              <div>
-              <video
-                width="500"
-                height="500"
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="h-auto max-w-full rounded-lg"
-              >
-                <source src="/assets/video2.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              </div>
-              <div>
-                  <Image
-                  width={500}
-                  height={500}
-                  priority
-                  className="h-auto max-w-full rounded-lg" 
-                  src="/assets/sample5.jpg" alt=""/>
-              </div>
-          </div>
-          <div className="grid gap-4">
-              <div>
-              <video
-                width="500"
-                height="500"
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="h-auto max-w-full rounded-lg"
-              >
-                <source src="/assets/video4.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              </div>
-              <div>
-                  <Image
-                  width={500}
-                  height={500}
-                  priority
-                  className="h-auto max-w-full rounded-lg" 
+                  className="h-72 object-cover object-top max-w-full rounded-lg" 
                   src="/assets/sample6.jpg" alt=""/>
               </div>
               <div>
@@ -109,7 +62,43 @@ const WebsiteDesign = () => {
                   width={500}
                   height={500}
                   priority
-                  className="h-auto max-w-full rounded-lg" 
+                  className="h-72 object-cover object-top max-w-full rounded-lg" 
+                  src="/assets/sample7.jpg" alt=""/>
+              </div>
+              <div>
+                  <Image
+                  width={500}
+                  height={500}
+                  priority
+                  className="h-72 object-cover object-top max-w-full rounded-lg" 
+                  src="/assets/sample8.jpg" alt=""/>
+              </div>
+          </div>
+
+          <div className="grid gap-4">
+              <div>
+                <Image
+                  width={500}
+                  height={500}
+                  priority
+                  className="h-72 object-cover object-top max-w-full rounded-lg"
+                  src="/assets/sample1.jpg" 
+                  alt=""/>
+              </div>
+              <div>
+                  <Image
+                  width={500}
+                  height={500}
+                  priority
+                  className="h-72 object-cover object-top max-w-full rounded-lg" 
+                  src="/assets/sample10.png" alt=""/>
+              </div>
+              <div>
+                  <Image
+                  width={500}
+                  height={500}
+                  priority
+                  className="h-72 object-cover object-top max-w-full rounded-lg" 
                   src="/assets/sample7.jpg" alt=""/>
               </div>
           </div>
